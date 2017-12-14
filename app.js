62ebd4c47ba5966fa7ca6b742c95c440d4cf0495
Dan Bateman <dbateman0319@starkstate.net>
2017-12-13 22:38:23 -0500

    Working with middleware

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 
 let app = express();
 
+//register partials to use for templates
+hbs.registerPartials(__dirname + '/views/partials');
+//set express configuration key value pair
+//view dir is default to render express views
+app.set('view engine', 'hbs');
+//middleware, use __dirname to point to this apps location then file
+//for static webpages, help.html
+app.use(express.static(__dirname + '/public'));
+//app use registers middleware and takes a function
+// next tells express when your middleware function is done
+app.use(function(req, res, next) {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log');
+		}
+	});
+	next();
+});
+
+app.use(function(req, res, next) {
+	res.render('maintenance.hbs');
+});
+
+//helper function
+hbs.registerHelper('getCurrentYear', function() {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function(text) {
+	return text.toUpperCase();
+});
+
+
+//routing
 app.get('/', function(req, res) {
 	//res.send('<h1>hello Express</h1>');
-	res.send({
-		name: 'Dan',
-		age: 25,
-		likes: ['programming', 'kids']
-	})
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		currentYear: new Date().getFullYear(),
+		welcomeMessage: 'Welcome!'
+
+	});
 });
 
 app.get('/about', function(req, res) {
-	res.send('About Page');
+	//render any templates with the current view engine
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+		currentYear: new Date().getFullYear()
+	});
 });
 
 app.get('/bad', function(req, res) {
@@ -22,4 +67,8 @@ app.get('/bad', function(req, res) {
 	});
 });
 
-app.listen(3000);
+
+
+app.listen(3000, function() {
+	console.log('Server is up on port 3000');
+});
